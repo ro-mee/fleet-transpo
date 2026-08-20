da9fc9d522e3d6dec2019091326798573bc335c0
ro-mee <lorenteromejoseph@gmail.com>
2026-08-20 18:24:56 +0800

    change color

--- mobile/app/(app)/(tabs)/index.js
+++ mobile/app/(app)/(tabs)/index.js
@@ -1273,11 +1273,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingVertical: moderateScale(12),
     paddingHorizontal: moderateScale(8),
-    shadowColor: "#000",
-    shadowOpacity: 0.08,
-    shadowRadius: 10,
-    shadowOffset: { width: 0, height: 4 },
-    elevation: 3,
   },
   quickGrid: {
     flexDirection: "row",
